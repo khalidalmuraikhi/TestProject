4d758ec81fdd7807d2a34adcd3dc78ad72cc16a4
khalid <khalid@pop-os.localdomain>
2020-09-28 10:48:01 +0300

    Added a Person class

--- TestProject/src/helloWorld.java
+++ TestProject/src/helloWorld.java
@@ -3,8 +3,20 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		
+		Person p1 = new Person("khalid", 25);
+		Person p2 = new Person("omar", 22);
+		Person p3 = new Person("fahad", 29);
+		
+		Person[] people = {p1,p2,p3};
+		
+		
+		for (Person person : people) {
+			System.out.println(person.toString());
+		}
 
+
+		
 	}
 
 }
